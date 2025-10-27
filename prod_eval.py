9209742f424a547e1a0da7777aa9f9e0b7607039
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2025-10-27 11:21:51 -0700

    Fixing bug
    Pushed from Domino: https://genai42demo.engineering-dev.domino.tech/workspace/integration-test/Haran-genai?executionId=68ff691c4bac6e55f6649b7d

--- prod_eval.py
+++ prod_eval.py
@@ -9,7 +9,7 @@ def hallucination_bool(p=0.8):
     return True if random.random() < p else False
 
 def log_dict_evals(trace_id, metrics_dict):
-    for k, v in metrics_dict:
+    for k, v in metrics_dict.items():
         log_evaluation(trace_id=trace_id, name=k, value=v)
 
 AI_SYSTEM_ID = "68ffb11c4bac6e55f6649d18"
@@ -18,5 +18,5 @@ for trace in traces.data:
     effort_rationale = trace.spans[0].outputs['final_score']['effort_rationale']
     request_description = trace.spans[0].inputs['ticket']['description']
     user_score = user_eval()
-    hallucination_bool = hallucination_bool()
-    log_dict_evals(trace_id=trace.id, metrics_dict={"user_score": user_score, "hallucination": hallucination_bool})
+    hallucination = hallucination_bool()
+    log_dict_evals(trace_id=trace.id, metrics_dict={"user_score": user_score, "hallucination": hallucination})

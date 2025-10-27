cb4e5b7434c28c03db60e77a9cd4c481d1a8e407
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2025-10-27 11:47:33 -0700

    Converted bool to label
    Pushed from Domino: https://genai42demo.engineering-dev.domino.tech/workspace/integration-test/Haran-genai?executionId=68ff691c4bac6e55f6649b7d

--- prod_eval.py
+++ prod_eval.py
@@ -6,17 +6,23 @@ def user_eval():
     return random.randint(1, 5)
 
 def hallucination_bool(p=0.8):
-    return True if random.random() < p else False
+    return 'True' if random.random() < p else 'False'
 
 def log_dict_evals(trace_id, metrics_dict):
     for k, v in metrics_dict.items():
         log_evaluation(trace_id=trace_id, name=k, value=v)
 
-AI_SYSTEM_ID = "68ffb11c4bac6e55f6649d18"
-traces = search_ai_system_traces(ai_system_id=AI_SYSTEM_ID)
-for trace in traces.data:
-    effort_rationale = trace.spans[0].outputs['final_score']['effort_rationale']
-    request_description = trace.spans[0].inputs['ticket']['description']
-    user_score = user_eval()
-    hallucination = hallucination_bool()
-    log_dict_evals(trace_id=trace.id, metrics_dict={"user_score": user_score, "hallucination": hallucination})
+if __name__=="__main__":
+    AI_SYSTEM_ID = "68ffb11c4bac6e55f6649d18"
+    traces = search_ai_system_traces(ai_system_id=AI_SYSTEM_ID)
+    count = 0
+    for trace in traces.data:
+        effort_rationale = trace.spans[0].outputs['final_score']['effort_rationale']
+        request_description = trace.spans[0].inputs['ticket']['description']
+        user_score = user_eval()
+        hallucination = hallucination_bool()
+        print(trace.id)
+        if trace.id is not None:
+            count += 1
+            print(count)
+        log_dict_evals(trace_id=trace.id, metrics_dict={"user_score": user_score, "hallucination": hallucination})
